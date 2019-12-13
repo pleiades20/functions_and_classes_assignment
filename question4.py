d1f4b61c9492a62db7fa7b730b18b71f0d9ab8f1
Kei Muneyasu <azuresky.clox4stream@gmail.com>
2019-12-13 21:52:04 +0900

    Complete.

--- question4.py
+++ question4.py
@@ -1,13 +1,23 @@
 #########IMPORT LIBRARIES HERE IF NEEDED#######
-import statistics
+
 ################END IMPORTS####################
 """
 Write a function that takes a list of numbers as input and returns the median of the list.
 """
 def f(l):
     ##########YOUR CODE HERE##########
-    median = statistics.median(l)
-    return median
+    N = len(l)
+    l.sort()
+    if N % 2 == 0:
+        median_a = N/2
+        median_b = N/2 + 1
+        median1 = int(median_a) - 1
+        median2 = int(median_b) - 1
+        return (l[median1] + l[median2]) / 2
+    else:
+        median_a = (N + 1) / 2
+        median1 = int(median_a) - 1
+        return l[median1]
 
 y = f(l)
 print(y)
