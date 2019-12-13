8f99a08b14c736fb6d72787f8bbab5113918cf2c
Kei Muneyasu <azuresky.clox4stream@gmail.com>
2019-12-13 20:42:36 +0900

    Complete.

--- question4.py
+++ question4.py
@@ -1,10 +1,14 @@
 #########IMPORT LIBRARIES HERE IF NEEDED#######
-
+import statistics
 ################END IMPORTS####################
 """
 Write a function that takes a list of numbers as input and returns the median of the list.
 """
 def f(l):
     ##########YOUR CODE HERE##########
-    pass
+    median = statistics.median(l)
+    return median
+
+y = f(l)
+print(y)
     ###########END CODE###############
